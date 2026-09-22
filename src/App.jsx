34bf735a0c68e578f402995adcadce4f4893564c
matyuswhitlock <you@example.com>
2026-09-21 19:46:06 -0500

    footer section and app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@
 /* --- YOUR IMPORTS GO HERE --- */
 
 import HeroSection from "./components/HeroSection";
+import FooterSection from "./components/FooterSection";
 
 // STEP 2: Create and export the App component
 // Use: export default function App() { ... }
@@ -101,7 +102,7 @@ export default function App(){
 
             {/* Footer */}
             <section className = "bg-footer">
-
+                 <FooterSection/>
             </section>
         </div>
     )
